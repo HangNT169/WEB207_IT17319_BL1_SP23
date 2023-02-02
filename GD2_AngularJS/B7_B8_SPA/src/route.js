f103d5723b173d8db82f34ee251be8e249bd7792
HangNT169 <HangNT169@fpt.edu.vn>
2023-02-02 15:53:45 +0700

    B7_SPA1

--- GD2_AngularJS/B7_B8_SPA/src/route.js
+++ GD2_AngularJS/B7_B8_SPA/src/route.js
@@ -1,26 +1,116 @@
 var myApp = angular.module("myModule", ["ngRoute"]);
-// Chuyen trang
+// chuyen trang
 myApp.config(function ($routeProvider, $locationProvider) {
+  // xoa khoang trang trinh duyet
   $locationProvider.hashPrefix("");
-  // chuyen trang
+  // $routeProvider : chuyen trang
   $routeProvider
-    .when("/home", {
-      // noi dung cua trang co duong dan la /trang-chu
-      // template: `<section>
-      //       <h1>Day la trang chu</h1>
-      //       <h1>Day la trang chu</h1>
-      //    </section>`,
-      templateUrl: "pages/trang-chu.html",
+    .when("/trang-chu", {
+      // lam gi thi lam
+      // noi dung cua trang chu
+      template: `<section>
+            <h1>Noi dung trang chu</h1>
+            <h1>Noi dung trang chu</h1>
+            <h1>Noi dung trang chu</h1>
+            <h1>Noi dung trang chu</h1>
+            <h1>Noi dung trang chu</h1>
+        </section>
+        <table>
+  <tr>
+    <th>Company</th>
+    <th>Contact</th>
+    <th>Country</th>
+  </tr>
+  <tr>
+    <td>Alfreds Futterkiste</td>
+    <td>Maria Anders</td>
+    <td>Germany</td>
+  </tr>
+  <tr>
+    <td>Centro comercial Moctezuma</td>
+    <td>Francisco Chang</td>
+    <td>Mexico</td>
+  </tr>
+</table><table>
+<tr>
+  <th>Company</th>
+  <th>Contact</th>
+  <th>Country</th>
+</tr>
+<tr>
+  <td>Alfreds Futterkiste</td>
+  <td>Maria Anders</td>
+  <td>Germany</td>
+</tr>
+<tr>
+  <td>Centro comercial Moctezuma</td>
+  <td>Francisco Chang</td>
+  <td>Mexico</td>
+</tr>
+</table><table>
+<tr>
+  <th>Company</th>
+  <th>Contact</th>
+  <th>Country</th>
+</tr>
+<tr>
+  <td>Alfreds Futterkiste</td>
+  <td>Maria Anders</td>
+  <td>Germany</td>
+</tr>
+<tr>
+  <td>Centro comercial Moctezuma</td>
+  <td>Francisco Chang</td>
+  <td>Mexico</td>
+</tr>
+</table><table>
+<tr>
+  <th>Company</th>
+  <th>Contact</th>
+  <th>Country</th>
+</tr>
+<tr>
+  <td>Alfreds Futterkiste</td>
+  <td>Maria Anders</td>
+  <td>Germany</td>
+</tr>
+<tr>
+  <td>Centro comercial Moctezuma</td>
+  <td>Francisco Chang</td>
+  <td>Mexico</td>
+</tr>
+</table><table>
+<tr>
+  <th>Company</th>
+  <th>Contact</th>
+  <th>Country</th>
+</tr>
+<tr>
+  <td>Alfreds Futterkiste</td>
+  <td>Maria Anders</td>
+  <td>Germany</td>
+</tr>
+<tr>
+  <td>Centro comercial Moctezuma</td>
+  <td>Francisco Chang</td>
+  <td>Mexico</td>
+</tr>
+</table>
+        `,
     })
-    .when("/gioi-thieu", {
-      // noi dung cua trang co duong dan la /trang-chu
-      template: "<h1>Day la gioi thieu</h1>",
+    .when("/contact", {
+      // lam gi thi lam
+      // noi dung cua trang chu
+      //   template: "<h1>Noi dung trang contact</h1>",
+      //Truyen file HTML
+      templateUrl: "pages/lien-he.html",
     })
-    .when("/about/HangNT169", {
-      // noi dung cua trang co duong dan la /trang-chu
-      template: "<h1>Day la chung toi</h1>",
+    .when("/cua-hang", {
+      // lam gi thi lam
+      // noi dung cua trang chu
+      template: "<h1>Noi dung trang cua hang</h1>",
     })
     .otherwise({
-      redirectTo: "/home",
+      redirectTo: "/trang-chu",
     });
 });
