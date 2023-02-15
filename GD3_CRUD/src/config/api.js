f7ec244604e9ac4dca48ab79b6b2553808ee2bb5
HangNT169 <HangNT169@fpt.edu.vn>
2023-02-15 10:29:57 +0700

    Finish CRUD

--- GD3_CRUD/src/config/api.js
+++ GD3_CRUD/src/config/api.js
@@ -1,7 +1,4 @@
 const url = "http://localhost:3000";
-
-// window.thucVatAPT = "http://localhost:3000/thucVat";
-// window.dongVatAPI = "http://localhost:3000/dongVat";
-// window.sinhVienAPI = "http://localhost:3000/sinhVien";
 window.thucVatAPI = url + "/thucVat";
-// => http://localhost:3000 /thucVat
+window.dongVatAPI = url + "/dongVat";
+window.sinhVienAPI = url + "/sinhVien";
--- GD3_CRUD/src/controller/home/thuc-vat-controller.js
+++ GD3_CRUD/src/controller/home/thuc-vat-controller.js
@@ -1,47 +1,66 @@
 window.ThucVatController = function ($scope, $http) {
-  // De giao tiep BE, FE dung phuong thuc HTTP de giao tiep
-  // Su dung cac method (HTTP method): GET, POST, PUT, DELETE,...
-  // CRUD : CREATE, READ, UPDATE, DELETE
-
-  // HTTP status code : 200,201,400,401,404,403,500...
-  // request: Nhung gia tri tu client => server (FE => BE)
-  // => Giong cai tham so truyen vao cua 1 function
-  // response : Ket qua tra ve tu phia server => client(BE => FE)
-  // => Ket qua tra ve cua 1 function
-  // GET : Hien thi du lieu <=> READ <=> SELECT
-  $scope.danhSachThucVatHienThi = [];
-  $scope.form_sinhVien = {
+  // De giao tiep BE, FE
+  // => Phuong thuc HTTP
+  // Voi cac HTTP Method : GET,POST,PUT,DELETE.....
+  // CRUD: CREATE, READ, UPDATE, DELETE
+  // request: Nhung gia tri tham so duoc truyen tu client => server
+  // (FE => BE) => Giong tham so truyen vao cua 1 function
+  // response: Gia tri nhan duoc tu phia server => client
+  // (BE=> FE)=> Giong nhu ket qua tra ve cua 1 function
+  // => Giong ket qua tra ve cua 1 function
+  // GET: Lay du lieu  <=> READ <=> SELECT
+  $scope.listThucVat = [];
+  $scope.form_thucVat = {
     ten: "",
     loai: "",
     gioiTinh: true,
   };
+  $scope.viTriUpdate = -1;
+  // -1 : Chua chon dong de update
   $http.get(thucVatAPI).then(function (response) {
-    // if (response.status === 200) {
-    //   $scope.danhSachThucVatHienThi = response.data;
-    // }
-    // console.log(response);
-    if (response.statusText === "OK") {
-      $scope.danhSachThucVatHienThi = response.data;
-    }
+    // console.log(response.data);
+    $scope.listThucVat = response.data;
   });
 
-  // DELETE : Xoa
-  $scope.removeStudent = function (event, index) {
+  // DELETE : Xoa du lieu <=> DELETE <=> DELETE
+  $scope.removeThucVat = function (event, index) {
     event.preventDefault();
-    let tv = $scope.danhSachThucVatHienThi[index];
+    let tv = $scope.listThucVat[index];
     let api = thucVatAPI + "/" + tv.id;
     $http.delete(api).then(function () {
-      $scope.danhSachThucVatHienThi.splice(index, 1);
-      alert("xoa thanh cong");
+      $scope.listThucVat.splice(index, 1);
+      alert("Xoa thanh cong");
     });
   };
 
   // Detail
-  $scope.detailStudent = function (event, index) {
+  $scope.detailThucVat = function (event, index) {
     event.preventDefault();
-    let tv = $scope.danhSachThucVatHienThi[index];
-    $scope.form_sinhVien.ten = tv.ten;
-    $scope.form_sinhVien.loai = tv.loai;
-    $scope.form_sinhVien.gioiTinh = tv.gioiTinh;
+    let tv = $scope.listThucVat[index];
+    $scope.form_thucVat.ten = tv.ten;
+    $scope.form_thucVat.loai = tv.loai;
+    $scope.form_thucVat.gioiTinh = tv.gioiTinh;
+    $scope.viTriUpdate = index;
+  };
+
+  // POST : Them du lieu <=> CREATE <=> INSERT INTO
+  $scope.addThucVat = function (event) {
+    event.preventDefault();
+    $http.post(thucVatAPI, $scope.form_thucVat).then(function (response) {
+      $scope.listThucVat.push(response.data);
+    });
+  };
+
+  // PUT: Sua du lieu <=> UPDATE <=> UPDATE
+  $scope.updateThucVat = function (event) {
+    event.preventDefault();
+    if ($scope.viTriUpdate == -1) {
+      alert("Chon di");
+    }
+    let tv = $scope.listThucVat[$scope.viTriUpdate];
+    let api = thucVatAPI + "/" + tv.id;
+    $http.put(api, $scope.form_thucVat).then(function (response) {
+      $scope.listThucVat[$scope.viTriUpdate] = response.data;
+    });
   };
 };
